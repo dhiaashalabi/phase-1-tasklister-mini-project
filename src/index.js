3efa8caef2e748aa1953408b57fa38d41daa6d44
Dhiaa Shalabi <dhiaa.shalabi@gmail.com>
2023-01-02 20:24:25 +0300

    Completed assignment.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const newTask = document.getElementById("new-task-description").value;
+    const newTaskLi = document.createElement("li");
+    newTaskLi.innerText = newTask;
+    document.getElementById("tasks").appendChild(newTaskLi);
+    document.getElementById("new-task-description").value = "";
+  });
 });
